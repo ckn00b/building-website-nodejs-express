5c124a27c988abed19818fe3661f0cea335f111c
ckn00b <cknew.dev@gmail.com>
2020-09-10 17:00:12 -0400

    added ESLint and Prettier to the code

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "./provided/static")));
+app.use(express.static(path.join(__dirname, './provided/static')));
 
-app.get("/", (request, response) => {
+app.get('/', (request, response) => {
   // response.send("Hello Express!!!");
-  response.sendFile(path.join(__dirname, "./provided/static/index.html"));
+  response.sendFile(path.join(__dirname, './provided/static/index.html'));
 });
 
-app.get("/speakers", (request, response) => {
-  response.sendFile(path.join(__dirname, "./provided/static/speakers.html"));
+app.get('/speakers', (request, response) => {
+  response.sendFile(path.join(__dirname, './provided/static/speakers.html'));
 });
 
 app.listen(port, () => {
