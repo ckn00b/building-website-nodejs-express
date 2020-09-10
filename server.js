af9cbd6b2302cd615a01fb314d5a513ae0278e9d
ckn00b <cknew.dev@gmail.com>
2020-09-10 17:02:55 -0400

    added nodemon

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 const port = 3000;
+
+// console.log('restarting');
 
 app.use(express.static(path.join(__dirname, './provided/static')));
 
